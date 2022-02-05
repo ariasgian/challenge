e865988a2aaf1ab4c54eb205233124be7f03a9fe
ariasgian <ariasgian@gmail.com>
2022-02-04 23:00:43 -0300

    generado los Dataframes

--- principal.py
+++ principal.py
@@ -7,9 +7,10 @@ Created on Sun Jan 30 18:19:14 2022
 from lxml import html
 import requests
 from datetime import datetime
-
-import csv
+import pandas as pd
 import os
+import io
+from fuzzywuzzy import fuzz
 
 #Funcion que realiza guardado del archivo
 def save(file_name, records):
@@ -21,10 +22,8 @@ def save(file_name, records):
     
     """
     check_dir(file_name)
-    csv_file = open(file_name,'a', newline='')
-    csvWriter = csv.writer(csv_file,delimiter=',')
-    for record in records:
-        csvWriter.writerow(record.decode('utf-8').split(','))
+    csv_file = open(file_name,'w', newline='', encoding='utf-8')
+    csv_file.write(records)
     print(" record saved to ",file_name)
     csv_file.close()
     return  None
@@ -51,9 +50,12 @@ def download(url, categoria):
     
     """
     response = requests.get(url)
-    año,mes,hoy_string=fecha()
-    path= categoria + "/" + str(año) +'-'+ mes+ "/" + categoria+ hoy_string+ '.csv'
-    save(path, response.iter_lines())# guarda datos segun el Path
+    response.encoding='utf-8'
+    
+    año,mes,hoy_string, hoy=fecha()
+    file= categoria + "/" + str(año) +'-'+ mes+ "/" + categoria+'-'+ hoy_string+ '.csv'
+    save(file, response.text)# guarda datos segun el Path
+    return file
 
 
 #Obtener la fecha en el formato dd-mm-yyyy
@@ -69,8 +71,8 @@ def fecha():
     hoy=datetime.now()
     año= hoy.year
     mes= meses[hoy.month]
-    hoy_string='-'+datetime.strftime(hoy, '%d-%m-%Y')
-    return año,mes,hoy_string
+    hoy_string=datetime.strftime(hoy, '%d-%m-%Y')
+    return año,mes,hoy_string,hoy
         
 
 
@@ -78,29 +80,57 @@ def fecha():
 #Diccionario con los nombre de los meses
 meses={1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 5:'Mayo', 6:'Junio',7:'Julio', 8:'Agosto',9: 'Septiembre', 10:'Octubre', 11:'Noviembre', 12: 'Diciembre'}
 
-# lista usada para definir a traves de XPath y pode descargar el link
-mat= [4,5,2]
-categorias=['museos', 'cine', 'bibliotecas']
+
 
 
 #Se hace primer request de la pagina donde descargar los datasets
 url='https://datos.gob.ar/dataset/cultura-mapa-cultural-espacios-culturales'
+
 page= requests.get(url)
+extractedHtml = html.fromstring(page.content)
 
+def name_colum(valor, patron):
+    for column_df in valor.columns:
+        
+        for column in patron:
+            a=fuzz.WRatio(column,column_df)
+            if a>92:
+                print(a, column,column_df)
+#%%
 
-#se hace un para descargar las 3 categorias 
-for i,v in enumerate(mat):
-    extractedHtml = html.fromstring(page.content)
-    link = extractedHtml.xpath(f'//*[@id="pkg-resources"]/div[{v}]/div/a[2]/@href')
-    download(link[0],categorias[i])
+def obtener_dataframes():
+    # lista usada para definir a traves de XPath y pode descargar el link
+    mat= [4,5,2]
+    categorias=['museos', 'cine', 'bibliotecas']
+    columns= ['Cod_Loc', 'IdProvincia', 'IdDepartamento', 
+           'categoria', 'provincia', 'localidad', 'nombre',
+           'direccion', 'CP', 'telefono', 'Mail', 'Web']
+    df=pd.DataFrame(columns=columns)
+    for i,v in enumerate(mat):
+        
+        link = extractedHtml.xpath(f'//*[@id="pkg-resources"]/div[{v}]/div/a[2]/@href')
+        page1= requests.get(link[0])
+        page1.encoding='utf-8'
+        download(link[0],categorias[i])
+        
+        data =pd.read_csv(io.StringIO(page1.text))
+        if categorias[i]=='cine':
+            df_cine=data
+        for column_df in data.columns:
+            for column in columns:
+                a=fuzz.WRatio(column,column_df)
+                if a>92:
+                    data.rename(columns={column_df:column}, inplace=True)
+        if 'Domicilio' in data.columns:
+            
+            data.rename(columns={'Domicilio':'direccion'}, inplace=True)
+        df=pd.concat([df,data[columns]])
+    return df_cine, df
     
-#sadadad
+       
 
-
-
-
-
-
-
-
+#%%
+df_cine,df=obtener_dataframes()  
+#%%
+df['fecha'] =fecha()[-1]       
 
